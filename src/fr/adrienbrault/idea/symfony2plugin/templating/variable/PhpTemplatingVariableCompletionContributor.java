2f53245906315325d0dc6e6dd925f9a832957a7e
Thomas Schulz <thomas.schulz@pfeifer-marketing.de>
2017-12-01 18:31:18 +0100

    New try on PHP template variable completion

--- src/fr/adrienbrault/idea/symfony2plugin/templating/variable/PhpTemplatingVariableCompletionContributor.java
+++ src/fr/adrienbrault/idea/symfony2plugin/templating/variable/PhpTemplatingVariableCompletionContributor.java
@@ -13,6 +13,7 @@ import com.jetbrains.php.completion.insert.PhpVariableInsertHandler;
 import com.jetbrains.php.lang.psi.elements.PhpNamedElement;
 import com.jetbrains.php.lang.psi.elements.Variable;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
+import com.jetbrains.php.lang.psi.stubs.indexes.PhpVariableIndex;
 import fr.adrienbrault.idea.symfony2plugin.templating.util.PhpTemplatingUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -29,14 +30,17 @@ public class PhpTemplatingVariableCompletionContributor extends CompletionContri
                 return;
             }
 
-//            PhpType type = new PhpType().add("#C\\Foo\\Bar\\PhpEngine");
-//            Project project = parameters.getPosition().getProject();
+            Project project = parameters.getPosition().getProject();
+            PhpIndex phpIndex = PhpIndex.getInstance(project);
+            PhpType type = new PhpType().add("#C" + PhpTemplatingUtil.getPhpEngineClass(phpIndex));
 //            InsertHandler insertHandler = (insertionContext, lookupElement) -> PhpVariableInsertHandler.getInstance().handleInsert(insertionContext, lookupElement);
 //            PhpLookupElement element = new PhpLookupElement("view", StubIndexKey.createIndexKey("TEMPLATING_VIEW"), PhpIcons.VARIABLE, type, project, insertHandler);
 //            result.addElement(element);
 
-            result.addElement(LookupElementBuilder.create("view"));
-            //result.addElement(LookupElementBuilder.create(new VariableImpl(new PhpVariableStubImpl(null, new PhpVariableElementType("dummy"), StringRef.fromString("dummyRef"), PhpType.INT))));
+            //LookupElementBuilder lookupElement = LookupElementBuilder.create("view");
+            //LookupElementBuilder lookupElement = LookupElementBuilder.create(new VariableImpl(new PhpVariableStubImpl(null, new PhpVariableElementType("dummy"), StringRef.fromString("dummyRef"), PhpType.INT)));
+            PhpLookupElement lookupElement = new PhpLookupElement("view", PhpVariableIndex.KEY, PhpIcons.VARIABLE, type, project, PhpVariableInsertHandler.getInstance());
+            result.addElement(lookupElement);
         }
     }
 }
