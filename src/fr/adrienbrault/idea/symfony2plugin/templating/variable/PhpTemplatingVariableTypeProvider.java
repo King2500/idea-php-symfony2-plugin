d26f575bf246960ed84fe699222e0e5792dc85a6
Thomas Schulz <thomas.schulz@pfeifer-marketing.de>
2017-12-05 18:43:15 +0100

    TypeProvider and resolving for PHP template variables from controller (WIP)

--- src/fr/adrienbrault/idea/symfony2plugin/templating/variable/PhpTemplatingVariableTypeProvider.java
+++ src/fr/adrienbrault/idea/symfony2plugin/templating/variable/PhpTemplatingVariableTypeProvider.java
@@ -1,7 +1,12 @@
 package fr.adrienbrault.idea.symfony2plugin.templating.variable;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
 import com.intellij.psi.search.LocalSearchScope;
 import com.jetbrains.php.PhpIndex;
 import com.jetbrains.php.lang.psi.PhpFile;
@@ -11,8 +16,11 @@ import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import com.jetbrains.php.lang.psi.resolve.types.PhpTypeProvider3;
 import fr.adrienbrault.idea.symfony2plugin.Symfony2ProjectComponent;
 import fr.adrienbrault.idea.symfony2plugin.templating.util.PhpTemplatingUtil;
+import fr.adrienbrault.idea.symfony2plugin.util.PsiElementUtils;
+import fr.adrienbrault.idea.symfony2plugin.util.VfsExUtil;
 import org.jetbrains.annotations.Nullable;
 
+import java.net.URL;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
@@ -55,14 +63,18 @@ public class PhpTemplatingVariableTypeProvider implements PhpTypeProvider3 {
 
         // class for $view, $app, ...
         // TODO: avoid index access !!
-        String varSignature = PhpTemplatingUtil.findSignatureForTemplateVariable(
-            (PhpFile)variable.getContainingFile(),
-            variable.getName()
-        );
+//        String varSignature = PhpTemplatingUtil.findSignatureForTemplateVariable(
+//            (PhpFile)variable.getContainingFile(),
+//            variable.getName()
+//        );
+//        if (varSignature != null) {
+//            return new PhpType().add("#" + this.getKey() + variable.getSignature() + TRIM_KEY + varSignature);
+//        }
 
         //String varSignature = variable.getName() + "~" + element.getContainingFile().
 
-        if (varSignature != null) {
+        if (element.getContainingFile().getVirtualFile() != null) {
+            String varSignature = variable.getName() + "~" + element.getContainingFile().getVirtualFile().getUrl();
             return new PhpType().add("#" + this.getKey() + variable.getSignature() + TRIM_KEY + varSignature);
         }
 
@@ -79,7 +91,29 @@ public class PhpTemplatingVariableTypeProvider implements PhpTypeProvider3 {
             return Collections.emptySet();
         }
 
-        return PhpIndex.getInstance(project).getAnyByFQN(expression.substring(endIndex + 1));
+        //return PhpIndex.getInstance(project).getAnyByFQN(expression.substring(endIndex + 1));
+        String parameter = expression.substring(endIndex + 1);
+        PhpIndex phpIndex = PhpIndex.getInstance(project);
+
+        int paramIndex = parameter.indexOf('~');
+        if (paramIndex != -1) {
+            String refFile = parameter.substring(paramIndex + 1);
+            String varName = parameter.substring(0, paramIndex);
+
+            VirtualFile relativeFile = VfsUtil.findRelativeFile(refFile, null);
+
+            if (relativeFile != null) {
+                PsiFile psiFile = PsiManager.getInstance(project).findFile(relativeFile);
+                if (psiFile != null && psiFile instanceof PhpFile) {
+                    String varSignature = PhpTemplatingUtil.findSignatureForTemplateVariable((PhpFile)psiFile, varName);
+                    if (varSignature != null) {
+                        return phpIndex.getAnyByFQN(varSignature);
+                    }
+                }
+            }
+        }
+
+        return Collections.emptySet();
     }
 
     private static boolean isLocalVariable(Variable variable) {
